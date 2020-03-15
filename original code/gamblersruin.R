9135e7b93b695c867b1974ffe4df3cac6e083540
Shane Kercheval <shane.kercheval@gmail.com>
2020-03-15 16:07:07 -0700

    update

--- original code/gamblersruin.R
+++ original code/gamblersruin.R
@@ -11,7 +11,7 @@
 gamble <- function(k,n,p) {
 	stake <- k
 	while (stake > 0 & stake < n) {
-		bet <- sample(c(-1,1),1,prob=c(1-p,p))
+		bet <- sample(c(-1,1), 1, prob=c(1-p, p))
 		stake <- stake + bet
 	}
 	if (stake == 0) return(1) else return(0)
@@ -23,4 +23,8 @@ p <- 1/2
 trials <- 1000
 simlist <- replicate(trials, gamble(k, n, p))
 mean(simlist) # Estimate of probability that gambler is ruined
+
 # For p = 0.5, exact probability is (n-k)/n 
+if(p == 0.5) {
+    (n-k)/n
+}
--- original code/pizza.R
+++ original code/pizza.R
@@ -6,13 +6,13 @@ trials <- 10000
 simlist <- numeric(trials)
 toppings <- c("pepper","pepperoni","pineapple","prawns","proscuitto")
 for (i in 1:trials) {
-pineapple <- 0
-pepperoni <- 0
-while (pineapple == 0) {
-   pick <- sample(toppings,1)
-   if (pick == "pepperoni") pepperoni <- pepperoni + 1
-   if (pick == "pineapple") pineapple <- 1
-   }	
-simlist[i] <- pepperoni
+    pineapple <- 0
+    pepperoni <- 0
+    while (pineapple == 0) {
+       pick <- sample(toppings,1)
+       if (pick == "pepperoni") pepperoni <- pepperoni + 1
+       if (pick == "pineapple") pineapple <- 1
+       }	
+    simlist[i] <- pepperoni
 }
 mean(simlist)
